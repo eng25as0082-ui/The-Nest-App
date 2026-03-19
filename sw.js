4464a0d2dcd6567a02690e91c89bb2cedba29677
Parnika K R <eng25as0082@dsu.edu.in>
2026-03-19 16:02:51 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE = 'nest-v1';
-const ASSETS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/manifest.json'];
+const V = 'nest-v3';
+const ASSETS = ['/', '/index.html', '/home.html', '/prompts.html', '/memory.html', '/family.html',
+  '/css/shared.css', '/css/login.css', '/css/home.css', '/css/prompts.css', '/css/memory.css', '/css/family.css',
+  '/js/state.js', '/js/login.js', '/js/home.js', '/js/prompts.js', '/js/memory.js', '/js/family.js',
+  '/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
 });
-
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
+  ));
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request)).catch(() => caches.match('/index.html'))
   );
 });
